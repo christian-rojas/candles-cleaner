828d2733d81f4c2be728be4f383342b365c35f3c
christian-rojas <christian.ici17@gmail.com>
2023-07-01 23:27:12 -0400

    add baseline deplot yaml

--- index.js
+++ index.js
@@ -6,17 +6,55 @@ const handler = async (event, context) => {
 
   try {
     await client.connect();
-    const database = client.db("pepper").collection('candles'); // Replace with your database name
+    const database = client.db("pepper").collection("candles"); // Replace with your database name
+
+    // const data = database.find({})
+    // const count = await data.sort({'data.insertDate':-1}).limit(10).toArray();
+
+    // const dates = await database.find({
+    //   insertDate: {
+    //       $lt: "2023-07-01 22:34:06.716333",
+    //   }
+    // }).sort({insertDate: -1}).toArray()
+    // Get the current date
+    // const currentDate = new Date();
+    // // Calculate one week behind
+    // const oneWeekBehind = new Date(currentDate.getTime() - 7 * 24 * 60 * 60 * 1000);
+
+    // console.log(oneWeekBehind);
+
+    const currentDate = new Date();
+
+    // Set a specific time zone
+    const targetTimeZone = "America/Santiago";
+    const options = { timeZone: targetTimeZone };
+
+    const oneWeekBehind = new Date(currentDate.getTime() - 7 * 24 * 60 * 60 * 1000);
+    // Format the date in the target time zone
+    const formattedDate = oneWeekBehind.toISOString("en-US", options);
     
-    const data = database.find({})
-    const count = await data.sort({'data.insertDate':-1}).limit(10).toArray();    
+
+    // console.log(oneWeekBehind);
+    
+
+    await database.deleteMany(
+      {
+        insertDate: {
+          $lt: formattedDate,
+        },
+      },
+      function (err, obj) {
+        if (err) throw err;
+        console.log("Number of documents deleted: " + obj.result.n);
+      }
+    );
     // for(const item of count) {
     //     let date = new Date(item.insertDate);
     //     date = date.toLocaleString();
     //     console.log(JSON.stringify(date));
     // }
-    console.log(JSON.stringify(count, null, 2));
-    
+    // console.log(JSON.stringify(dates, null, 2));
+
     return {
       statusCode: 200,
       body: JSON.stringify({ message: "Data listed successfully" }),
@@ -33,4 +71,4 @@ const handler = async (event, context) => {
   }
 };
 
-// handler();
+handler();
